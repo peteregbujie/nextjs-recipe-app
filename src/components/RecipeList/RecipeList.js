c2b451f93c2d3c72ca16f31c2c325c6671fc86cf
Peter Egbujie <ogbillion@gmail.com>
2022-01-04 01:35:23 -0800

    Removed Categories Folder

--- src/components/RecipeList/RecipeList.js
+++ src/components/RecipeList/RecipeList.js
@@ -1,16 +1,16 @@
-
-function RecipeList({recipes}) {
+import styles from './RecipeList.module.css';
+function RecipeList({ recipes }) {
     return (
-     <>
-      {recipes.map((recipe) => (
-       <Link href={`/recipe/${recipe.idMeal}`} key={recipe.strMeal}>
-        <div className={styles.card}></div>
-       </Link>
-      ))}
-     </>
+        <>
+            {recipes.map((recipe) => (
+                <Link href={`/recipe/${recipe.idMeal}`} key={recipe.strMeal}>
+                    <div className={styles.card}></div>
+                </Link>
+            ))}
+        </>
     );
-}
 
+}
 export default RecipeList
 
 
