798cc127fa5857d6d6a027b8f441f16fb94646eb
Peter Egbujie <ogbillion@gmail.com>
2022-06-21 01:08:26 -0700

    Updated global styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,23 @@
 import axios from "axios";
-import { useState } from 'react';
-import useSWR from 'swr';
+import { useState } from "react";
+import useSWR from "swr";
 import Hero from "../components/Hero/Hero";
-import Layout from '../components/Layout/Layout';
-import RecipeList from '../components/RecipeList/RecipeList';
-import { fetchMeals } from '../lib/requests';
+import Layout from "../components/Layout/Layout";
+import RecipeList from "../components/RecipeList/RecipeList";
+import { fetchMeals } from "../lib/requests";
 
-export default function Home({ randomRecipes }) {  
-  const [searchTerm, setSearchTerm] = useState('');
+export default function Home({ randomRecipes }) {
+ const [searchTerm, setSearchTerm] = useState("");
 
- const updateRecipe = recipe => setSearchTerm(recipe); 
-  
-  
+ const updateRecipe = (recipe) => setSearchTerm(recipe);
+
  const searchUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`;
 
  const fetcher = async (url) => await axios.get(url).then((res) => res.data);
  const { data: searchResult, error } = useSWR(searchUrl, fetcher);
 
-   
- if (error) return <p>Something went wrong!</p>;
- if (!searchResult) return <h1>Loading...</h1>;
- 
+ if (error) return <span>Something went wrong!</span>;
+ if (!searchResult) return <span>Loading...</span>;
 
  return (
   <Layout>
@@ -32,10 +29,7 @@ export default function Home({ randomRecipes }) {
  );
 }
 
-
-
 export const getStaticProps = async () => {
- 
  const randomRecipes = await fetchMeals();
 
  return {
