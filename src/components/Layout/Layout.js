934e5e627eeaed6612be468ac83f65bb9c4bcf8c
Peter Egbujie <ogbillion@gmail.com>
2022-06-21 01:10:35 -0700

    Updated Layout and Navbar styling

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,21 @@
+import Head from "next/head";
 import Navbar from "../Navbar/Navbar";
-import styles from './Layout.module.css';
+import styles from "./Layout.module.css";
 
-const Layout = ({ children}) => {
-        
-    return (
-     <div className={styles.container}>     
-      <Navbar />
-      <main className={styles.main}>{children}</main>
-      <footer className={styles.footer}>Recipe Finder</footer>
-     </div>
-    );
+const Layout = ({ children }) => {
+ return (
+  <div className={styles.container}>
+   <Head>
+    <title>Recipe Finder</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charSet="utf-8" />
+    <link rel="icon" href="/favicon.ico"></link>
+   </Head>
+   <Navbar />
+   <main className={styles.main}>{children}</main>
+   <footer className={styles.footer}>Recipe Finder</footer>
+  </div>
+ );
 };
 
 export default Layout;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,52 +1,33 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import { useState } from 'react';
-import { FaBars, FaTimes } from 'react-icons/fa';
-import styles from './Navbar.module.css';
+import Link from "next/link";
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import styles from "./Navbar.module.css";
 
-
-const Navbar =({title = "Recipe Finder"})=> {
+const Navbar = () => {
  const [isOpen, setisOpen] = useState(false);
  const openMenu = () => setisOpen(!isOpen);
  return (
-     <div className={styles.head}>
-   <nav className={styles.navbar}>
-    <Head>
-     <title>{title}</title>
-     <link rel='icon' href='/favicon.ico'></link>
-    </Head>
-    <header>
-     <a href='#' className={styles.navbar__logo}>
-      RecipeFinder
-     </a>
-    </header>
-    <ul className={isOpen ? styles.navbar__menu.active : styles.navbar__menu}>
-     <li className={styles.navbar__item}>
-      <Link href='/'>
-       <a>Home</a>
-      </Link>
-     </li>
-     <li className={styles.navbar__item}>
-      <Link href='/'>
-       <a>Categories</a>
-      </Link>
-     </li>
-     <li className={styles.navbar__item}>
-      <Link href='/'>
-       <a>Recipes</a>
-      </Link>
-     </li>
-    </ul>
-    <div className={styles.navbar__hamburger} onClick={openMenu}>
-     {isOpen ? (
-      <FaTimes size={30} style={{ color: '#f8f8f8' }} />
-     ) : (
-      <FaBars size={30} style={{ color: '#f8f8f8' }} />
-     )}
-    </div>
-   </nav>
-  </div>
+  <nav className={styles.navbar}>
+   <Link href="/">
+    <a
+     style={{
+      fontSize: 30,
+      fontWeight: "bold",
+      paddingBottom: "1rem",
+     }}
+    >
+     Home
+    </a>
+   </Link>
+   <div className={styles.navbar__hamburger} onClick={openMenu}>
+    {isOpen ? (
+     <FaTimes size={30} style={{ color: "#f8f8f8" }} />
+    ) : (
+     <FaBars size={30} style={{ color: "#f8f8f8" }} />
+    )}
+   </div>
+  </nav>
  );
-}
+};
 
 export default Navbar;
