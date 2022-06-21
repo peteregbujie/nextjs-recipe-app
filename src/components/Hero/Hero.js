c3a99d73b926c0539a86af4707f7391a5fe05f43
Peter Egbujie <ogbillion@gmail.com>
2022-06-20 20:22:25 -0700

    Fixed Navbar and Hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,35 +1,35 @@
-
-import { useState } from 'react';
-import { FaSearch } from 'react-icons/fa';
-import styles from './Hero.module.css';
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import styles from "./Hero.module.css";
 
 const Hero = ({ updateRecipe }) => {
-  const [recipe, setRecipe] = useState("");
+ const [recipe, setRecipe] = useState("");
 
-  const handleChnage = (e) => setRecipe(e.target.value)
-  
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    updateRecipe(recipe)
-    setRecipe("");
-};
-  
+ const handleChange = (e) => setRecipe(e.target.value);
+
+ const handleSubmit = (e) => {
+  e.preventDefault();
+  updateRecipe(recipe);
+  setRecipe("");
+ };
+
  return (
-  <form onSubmit={handleSubmit} className={styles.hero}>
-   <div className={styles.hero__text}>Find Your favorite Recipes</div>
-   <div className={styles.hero__input}>
-    <FaSearch size={30} />
-    <input
-     type='text'
-     value={recipe}
-     placeholder='Search for Recipes'
-     onChange={handleChnage}
-    />
-    <button type='submit'>search</button>
-   </div>
-  </form>
+  <>
+   <form onSubmit={handleSubmit} className={styles.hero}>
+    <div className={styles.hero__text}>Find Your favorite Recipes</div>
+    <div className={styles.hero__input}>
+     <FaSearch size={30} />
+     <input
+      type="text"
+      value={recipe}
+      placeholder="Search for Recipes"
+      onChange={handleChange}
+     />
+     <button type="submit">search</button>
+    </div>
+   </form>
+  </>
  );
 };
 
-
-export default Hero
+export default Hero;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,52 +1,33 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import { useState } from 'react';
-import { FaBars, FaTimes } from 'react-icons/fa';
-import styles from './Navbar.module.css';
+import Head from "next/head";
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import styles from "./Navbar.module.css";
 
-
-const Navbar =({title = "Recipe Finder"})=> {
+const Navbar = ({ title = "Recipe Finder" }) => {
  const [isOpen, setisOpen] = useState(false);
  const openMenu = () => setisOpen(!isOpen);
  return (
-     <div className={styles.head}>
+  <div className={styles.head}>
    <nav className={styles.navbar}>
     <Head>
      <title>{title}</title>
-     <link rel='icon' href='/favicon.ico'></link>
+     <link rel="icon" href="/favicon.ico"></link>
     </Head>
     <header>
-     <a href='#' className={styles.navbar__logo}>
+     <a href="#" className={styles.navbar__logo}>
       RecipeFinder
      </a>
     </header>
-    <ul className={isOpen ? styles.navbar__menu.active : styles.navbar__menu}>
-     <li className={styles.navbar__item}>
-      <Link href='/'>
-       <a>Home</a>
-      </Link>
-     </li>
-     <li className={styles.navbar__item}>
-      <Link href='/'>
-       <a>Categories</a>
-      </Link>
-     </li>
-     <li className={styles.navbar__item}>
-      <Link href='/'>
-       <a>Recipes</a>
-      </Link>
-     </li>
-    </ul>
     <div className={styles.navbar__hamburger} onClick={openMenu}>
      {isOpen ? (
-      <FaTimes size={30} style={{ color: '#f8f8f8' }} />
+      <FaTimes size={30} style={{ color: "#f8f8f8" }} />
      ) : (
-      <FaBars size={30} style={{ color: '#f8f8f8' }} />
+      <FaBars size={30} style={{ color: "#f8f8f8" }} />
      )}
     </div>
    </nav>
   </div>
  );
-}
+};
 
 export default Navbar;
